49cfa5706e27e15009576d1603a51a19b19061a2
Håvard <havardd@365.had.no>
2025-01-24 16:35:02 +0100

    Create standardverdier.js

---
+++ standardverdier.js
@@ -0,0 +1,329 @@
+const standardverdier = {
+  title: "Prosjektstyring ved hjelp av Prosjektveiviseren - Prosessmodell",
+  header1: "Prosjektstyring ved hjelp av Prosjektveiviseren - Prosessmodell",
+  header2: "Den anbefalte prosjektmodellen for offentlig sektor",
+  virksomhetsstyring: {
+    title: "Virksomhetsstyring",
+    highlighted: "Virksomhetsstyringen tilhører de blå fasene.",
+    normal: "Linejeorganisasjonenes gjennomføring av prosjekter et viktig virkemiddel for å realisere sine strategiske mål. Prosjektstyring sikrer styring og kontoll på de endringene som blir avtalt.",
+    linkText: "Les mer",
+    linkHref: "newpage.html",
+    padriver: {
+      text: "Roller",
+      linkText: "Les mer",
+      linkHref: "newpage.html"
+    },
+    eier: {
+      text: "Ansvar",
+      linkText: "Les mer",
+      linkHref: "newpage.html"
+    },
+    ansvar: {
+      text: "Arbeidsform",
+      linkText: "Les mer",
+      linkHref: "newpage.html"
+    }
+  },
+  prosjektstyring: {
+    title: "Prosjektstyring",
+    highlighted: "Prosjektets faser er grønne, og er prosjekteierens ansvar.",
+    normal: "Virksomhetsledelsen oppnevner en prosjekteier som i prosjektperioden ivaretar overordnet overvåkning, styring og støtte til prosjektleder.",
+    linkText: "Les mer",
+    linkHref: "newpage.html",
+    padriver: {
+      text: "Roller",
+      linkText: "Les mer",
+      linkHref: "newpage.html"
+    },
+    eier: {
+      text: "Ansvar",
+      linkText: "Les mer",
+      linkHref: "newpage.html"
+    },
+    ansvar: {
+      text: "Arbeidsform",
+      linkText: "Les mer",
+      linkHref: "newpage.html"
+    }
+  },
+  fases: [
+    {
+      title: "Idé",
+      highlighted: "A",
+      normal: "B",
+      linkText: "Les mer",
+      linkHref: "newpage.html",
+      padriver: {
+        text: "Den som har idéen",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      eier: {
+        text: "Den som bestemmer hva man skal bruke tid og penger på",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      ansvar: {
+        text: "Prosjektansvarlig",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      }
+    },
+
+        {
+      title: "Konsept",
+      highlighted: "A",
+      normal: "B",
+      linkText: "Les mer",
+      linkHref: "newpage.html",
+      padriver: {
+        text: "Leder av utredningsarbeidet",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      eier: {
+        text: "Eier",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      ansvar: {
+        text: "Arbeidsform",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      }
+    },
+
+        {
+      title: "Planlegge",
+      highlighted: "A",
+      normal: "B",
+      linkText: "Les mer",
+      linkHref: "newpage.html",
+      padriver: {
+        text: "Prosjektleder",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      eier: {
+        text: "Projekteier",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      ansvar: {
+        text: "Arbeidsform",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      }
+    },
+
+        {
+      title: "Gjennomføre",
+      highlighted: "A",
+      normal: "B",
+      linkText: "Les mer",
+      linkHref: "newpage.html",
+      padriver: {
+        text: "Prosjektleder",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      eier: {
+        text: "Projekteier",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      ansvar: {
+        text: "Arbeidsform",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      }
+    },
+
+        {
+      title: "Avslutte",
+      highlighted: "A",
+      normal: "B",
+      linkText: "Les mer",
+      linkHref: "newpage.html",
+      padriver: {
+        text: "Prosjektleder",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      eier: {
+        text: "Projekteier",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      ansvar: {
+        text: "Arbeidsform",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      }
+    },
+
+        {
+      title: "Realisere",
+      highlighted: "A",
+      normal: "B",
+      linkText: "Les mer",
+      linkHref: "newpage.html",
+      padriver: {
+        text: "Gevinsteier",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      eier: {
+        text: "Gevinstansvarlig",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      ansvar: {
+        text: "Arbeidsform",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      }
+    },
+
+  ],
+  besokspunkt: [
+    // Besøkspunkt med padriver, eier og ansvar
+    {
+      title: "BP1",
+      highlighted: "Beslutningspunkt 1",
+      normal: "Beslutte oppstart av en konseptfase",
+      linkText: "Les mer",
+      linkHref: "newpage.html",
+      padriver: {
+        text: "Pådriver",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      eier: {
+        text: "Eier",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      ansvar: {
+        text: "Arbeidsform",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      }
+    },
+
+    {
+      title: "BP2",
+      highlighted: "Beslutningspunkt 2",
+      normal: "Beslutte oppstart av en konseptfase",
+      linkText: "Les mer",
+      linkHref: "newpage.html",
+      padriver: {
+        text: "Pådriver",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      eier: {
+        text: "Eier",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      ansvar: {
+        text: "Arbeidsform",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      }
+    },
+
+    {
+      title: "BP3",
+      highlighted: "Beslutningspunkt 3",
+      normal: "Beslutte oppstart av en konseptfase",
+      linkText: "Les mer",
+      linkHref: "newpage.html",
+      padriver: {
+        text: "Pådriver",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      eier: {
+        text: "Eier",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      ansvar: {
+        text: "Arbeidsform",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      }
+    },
+
+    {
+      title: "BP4",
+      highlighted: "Beslutningspunkt 4",
+      normal: "Beslutte oppstart av en konseptfase",
+      linkText: "Les mer",
+      linkHref: "newpage.html",
+      padriver: {
+        text: "Pådriver",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      eier: {
+        text: "Eier",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      ansvar: {
+        text: "Arbeidsform",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      }
+    },
+
+    {
+      title: "BP5",
+      highlighted: "Beslutningspunkt 5",
+      normal: "Beslutte oppstart av en konseptfase",
+      linkText: "Les mer",
+      linkHref: "newpage.html",
+      padriver: {
+        text: "Pådriver",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      eier: {
+        text: "Eier",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      ansvar: {
+        text: "Arbeidsform",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      }
+    },
+
+    {
+      title: "BP6",
+      highlighted: "Beslutningspunkt 6",
+      normal: "Beslutte oppstart av en konseptfase",
+      linkText: "Les mer",
+      linkHref: "newpage.html",
+      padriver: {
+        text: "Pådriver",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      eier: {
+        text: "Eier",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      },
+      ansvar: {
+        text: "Arbeidsform",
+        linkText: "Les mer",
+        linkHref: "newpage.html"
+      }
+    },
+  ]
+};
